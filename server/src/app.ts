d73aeb6962d39538ee3598cbcacea8455950b663
sagiede <sagiede@post.bgu.ac.il>
2019-03-27 22:19:33 +0200

    locate eviroments in Env - path support

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 import express = require('express');
-
+const path = require('path');
 // import { EDESTADDRREQ } from 'constants';
 // import { usersApi } from './usersApi/usersApi';
 import {usersApiRouter} from './usersApi/userRoutes';
-require('dotenv').config();
+const bodyParser = require('body-parser');
 
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 const dbHost = process.env.DB_HOST;
 const dbName = process.env.DB_NAME;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+// debugger;
 console.log('connection to :' + dbName);
 mongoose.connect('mongodb+srv://'+dbUser+':'+dbPassword+'@'+dbHost+'/'+dbName+
                     '?retryWrites=true', {useNewUrlParser: true});
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(usersApiRouter);
 //app.use(storesApiRouter);
 //app.use(productsApiRouter);
