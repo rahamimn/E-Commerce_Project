a36fa6bef97d195ef57347bf5e6effe5955fd30f
sagiede <sagiede@post.bgu.ac.il>
2019-03-30 23:57:09 +0300

    basic productsApi and solving conflicts

---
+++ server/src/productApi/productRoutes.ts
@@ -0,0 +1,8 @@
+import {ProductsApi} from "./productsApi";
+import express = require('express');
+// import * as Constants from "../consts";
+// import {createToken, verifyToken} from "../jwt";
+
+export const productsApiRouter = express.Router();
+
+const productsApi = new ProductsApi();
