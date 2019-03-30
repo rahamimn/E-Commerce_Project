e6e2b7658240cdc880ba57a1aac73f0c7d33133f
Adir Oungaer <adiro@wix.com>
2019-03-30 12:14:36 +0300

    updated interface

--- server/src/usersApi/users.d.ts
+++ server/src/usersApi/users.d.ts
@@ -5,12 +5,13 @@ export interface UsersApi{
     logout: (userName: string) => number,
     delete: () => void,
     update: () => void,
-    setUserAsSystemAdmin: () => void,
-    setUserAsStoreOwner: () => void,
-    setUserAsStoreManager: () => void,
-    addProductToCart:(userId: string, storeID: string, productId: string, amount: number) => number,
+    setUserAsSystemAdmin: (userId:String, appointedUserId:String) => Promise<number>,
+    setUserAsStoreOwner: (userId:String, appointedUserId:String, storeId:String) => Promise<number>,
+    setUserAsStoreManager: (userId:String, appointedUserId:String, storeId:String, permissions:String[]) => Promise<number>,
+    addProductToCart:(userId:String,storeId: String, productId: String, amount: Number) => Promise<number>,
     sendMessage: () => void,
     getMessages: () => void,
-    getNotifications: () => void,
-    removeRole: (userId:number , userIdRemove:number, storeIdRemove:number) => Promise<void>
+    popNotifications: (userId:String) => Promise<String[]>,
+    removeRole: (userId:number , userIdRemove:number, storeIdRemove:number) => Promise<void>,
+    updatePermissions: (userId:String, appointedUserId:String, storeId:String, permissions: String[]) => Promise<number>
 }
