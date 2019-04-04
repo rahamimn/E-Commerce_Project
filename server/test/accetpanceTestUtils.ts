be4858e05dd0562f8b62c32cb3f6b60219598ba0
Adir Oungaer <adiro@wix.com>
2019-04-05 00:37:45 +0300

    login admin for acceptanceTest

--- server/test/accetpanceTestUtils.ts
+++ server/test/accetpanceTestUtils.ts
@@ -2,11 +2,16 @@ import { UserCollection, RoleCollection } from "../src/persistance/mongoDb/Colle
 import { fakeUser, fakeRole } from "./fakes";
 import { RoleModel } from "../src/persistance/mongoDb/models/roleModel";
 import { ADMIN } from "../src/consts";
+import bcrypt = require('bcryptjs');
 
 export const setDefaultData = async () => {
+    const salt = bcrypt.genSaltSync(10);
+    const password = ' 1234'
+    const hashed =  bcrypt.hashSync(password+process.env.HASH_SECRET_KEY, salt);
     const user = await UserCollection.insert(fakeUser({
         userName:'admin1234',
-        password:'1234'
+        password:hashed,
+        salt
     }));
     const role = await RoleCollection.insert(fakeRole({
         name: ADMIN,
