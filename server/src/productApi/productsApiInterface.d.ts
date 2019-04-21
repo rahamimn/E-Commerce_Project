31b455d4fee0ed36fa34f63ef0e98f01c88a7572
rahamimn <rahamimn@post.bgu.ac.il>
2019-04-21 17:37:57 +0300

    producApiInterface FIX

--- server/src/productApi/productsApiInterface.d.ts
+++ server/src/productApi/productsApiInterface.d.ts
@@ -4,7 +4,7 @@ import { Product } from './models/product';
 
 export interface IProductApi{
 
-    addProduct: (userId: String, storeId: String,name:String, amountInventory: number, sellType: String, price: number, keyWords: String[], category: String) =>  Promise< { status: number ,error?: String, product?:Product   }>,
+    addProduct: (userId: String, storeId: String, productName:String, amountInventory: number, sellType: String, price: number, keyWords: String[], category: String) =>  Promise< { status: number ,error?: String, product?:Product   }>,
     removeProduct: (userId: String, productId: String) => Promise< { status: number ,error?: String, product?:Product   }>,
     updateProduct: (userId: String, storeId: String, productId: String, newPrice: number) => Promise< { status: number ,error?: String, product?:Product   }>,
     getProducts: (filter:{storeId?:String, category?: String, keyWords?: String[], name?:String}) => Promise< {status:number, error?: String, products?:Product[]   }>, 
